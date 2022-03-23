bc159e7a2b946c1a14231b793d4331ca1f5d34ed
ajoy267 <andrew.joy12@hotmail.com>
2022-03-23 15:06:49 -0700

    passed test 8

--- exercise-8/exercise-8.sql
+++ exercise-8/exercise-8.sql
@@ -1,2 +1,24 @@
 -- rank of film category by times rented
 -- include the name, count, and rank
+SELECT 
+    name,
+    COUNT(*), 
+    RANK() OVER(ORDER BY COUNT(*) DESC)
+FROM 
+    category
+RIGHT JOIN 
+    film_category
+ON 
+    category.category_id = film_category.category_id
+RIGHT JOIN 
+    inventory
+ON 
+    film_category.film_id = inventory.film_id
+RIGHT JOIN 
+    rental
+ON 
+    inventory.inventory_id = rental.inventory_id
+GROUP BY 
+    name
+ORDER BY 
+    RANK
